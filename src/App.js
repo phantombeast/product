cdbf2f3eee15670abee96528acac48852f161c01
phantombeast <rajpalpulkit12@gmail.com>
2020-09-28 22:22:57 +0530

    http

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Views/Home'
 import About from './Views/About'
-
+import Product from './Views/Product'
 
 import './App.css';
 
@@ -25,9 +25,14 @@ function App() {
         <Route exact path="/">
          <Home/>
         </Route>
+
         <Route path="/about">
        <About/>
         </Route>
+        
+        <Route path="/products/:id">
+            <Product />
+          </Route>
       </Switch>
 
       <Footer/>  
